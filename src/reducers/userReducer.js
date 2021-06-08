3c392f503312cc82d41eca730b3a917356b2a046
IJMilk-WS <rladntjd320@gmail.com>
2021-06-09 01:34:49 +0900

    [add] : #2/ kings-ears-client / edit project init

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,13 +1,7 @@
 import { initialState } from './initialState';
-import { SIGN_IN } from '../actions';
 
 const userReducer = (state = initialState, action) => {
     switch(action.type) {
-        case SIGN_IN:
-            return {
-                ...state,
-                userinfo: action.payload
-            }
         default:
             return state; 
     }
