45b3e223d5b395d8394a9b4eeb8b27bb0a3d0e69
IJMilk-WS <rladntjd320@gmail.com>
2021-06-09 20:16:38 +0900

    [modify] : #5 / reducers and actions for redux / modify and add actions, reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,30 @@
-export const SIGN_IN = 'SIGN_IN';
+export const GET_ACCESS_TOKEN = 'GET_ACCESS_TOKEN';
+export const IS_LOGIN = 'IS_LOGIN';
+export const GET_USER_LEVEL = 'GET_USER_LEVEL';
 
-export const singIn = () => {
+export const getAccessToken = (accessToken) => {
     return {
-        type: SIGN_IN,
+        type: GET_ACCESS_TOKEN,
         payload: {
-            userinfo: '',
+            accessToken: accessToken,
+        }
+    }
+}
+
+export const isLogin = (status) => {
+    return {
+        type: IS_LOGIN,
+        payload: {
+            isLogin: status,
+        }
+    }
+}
+
+export const getUserLevel = (userLevel) => {
+    return {
+        type: GET_USER_LEVEL,
+        payload: {
+            userLevel: userLevel,
         }
     }
 }
