36ff31fa6f9269b962628fa165239e35e770ef18
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-06-09 22:57:03 +0900

    [Create]: #7 / Added two button boxes (members and visitors) components to be rendered on the landing page

---
+++ src/components/memberButtons/MemberButtons.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function MemberButtons() {
+  return (
+    <div className="landing-modal-buttons">
+      <button>비밀보기</button>
+      <button>비밀쓰기</button>
+    </div>
+  )
+}
