a1de9df6fd786e68ca4e093a6e2d25658c365dfe
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-06-16 09:12:36 +0900

    [Update]: #47 / Added tooltips on nav that show when hovered

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,6 +4,7 @@ import './style.css'
 import { HiOutlineLogout } from 'react-icons/hi'
 import { ImHome3 } from 'react-icons/im'
 import { RiUserHeartLine } from 'react-icons/ri'
+import Tooltip from '../tooltip/Tooltip'
 
 export default function Nav() {
   function handleLogOut() {
@@ -12,9 +13,18 @@ export default function Nav() {
   return (
     <nav className="Nav">
       <ul>
-        <li><Link to="/"><ImHome3/></Link></li>
-        <li><Link to="/mypage"><RiUserHeartLine/></Link></li>
-        <li><HiOutlineLogout onClick={handleLogOut}/></li>
+        <li>
+          <Link to="/"><ImHome3/></Link>
+          <Tooltip text={`홈`}/>
+        </li>
+        <li>
+          <Link to="/mypage"><RiUserHeartLine/></Link>
+          <Tooltip text={`마이페이지`}/>
+        </li>
+        <li>
+          <HiOutlineLogout onClick={handleLogOut}/>
+          <Tooltip text={`로그아웃`}/>
+        </li>
       </ul>
     </nav>
   )
---
+++ src/components/tooltip/Tooltip.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export default function Tooltip( {text} ) {
+  return (
+    <div className="Tooltip">{text}</div>
+  )
+}
